71fbf69adb9e144957cdcd4d214145bd083e7e89
straussdave <101413861+straussdave@users.noreply.github.com>
2023-07-03 17:55:36 +0200

    implemented logging

--- SWEN_TourPlanner/ViewModel/MainViewModel.cs
+++ SWEN_TourPlanner/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using log4net.Appender;
 using SWEN_TourPlanner.Model;
 using System;
 using System.Collections.ObjectModel;
@@ -10,40 +11,24 @@ namespace SWEN_TourPlanner.ViewModel
 {
     public partial class MainViewModel : ObservableObject
     {
+        Logger logger = new();
+
         int counter;
         [ObservableProperty] //this auto generates code in Dependencies/net7.0-windows/CommunityToolkin.Mvvm.SourceGenerators/CoomunityToolkit.MvvmSourceGenerators.ObservablePropertyGenerator
         string text;
         
         DatabaseHandler handler = new();
 
-        [RelayCommand]
+        [RelayCommand] //example command
         void Clicked()
         {
             counter++;
             Text = counter.ToString();
         }
 
-        //example data for the datagrid
-
-        static Random random = new();
-        public ObservableCollection<Student> Items { get; } = new();
         public MainViewModel()
         {
-            for (int i = 0; i < 10; i++)
-            {
-                Items.Add(new Student
-                {
-                    Id = i,
-                    Name = "Person " + i,
-                    Age = random.Next(14, 85),
-                });
-            }
-        }
-        public class Student
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public int Age { get; set; }
+            logger.log.Debug("MainViewModel created");
         }
     }
 }
